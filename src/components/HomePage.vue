e125b502b9ec229e658a0a11fabdcc96682d8bb8
Rutvij Sathe <rut26sathe@gmail.com>
2021-01-06 18:57:56 +0530

    Skills Card

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -2,10 +2,11 @@
 <div>
 <section class="Sec">
   <Education />
-  <Work />
+  <Projects />
+  <Skill />
 </section>
 <section class="Sec">
-<Projects />
+  <Work />
 </section>
 </div>
 </template>
@@ -14,12 +15,14 @@
 import Education from "./Cards/Education";
 import Work from "./Cards/Work.vue";
 import Projects from "./Cards/Projects.vue";
+import Skill from "./Cards/Skills.vue"
 export default {
   name: "home",
   components: {
     Education,
     Work,
     Projects,
+    Skill
   },
 };
 </script>
