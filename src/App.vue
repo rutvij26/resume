3ceac9f60292e88f81da92affac30e61aad40665
Rutvij Sathe <rut26sathe@gmail.com>
2021-01-06 13:34:57 +0530

    Home Component Created

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
  <v-app>
    <NavBar />
+   <Home />
  </v-app>
 </template>
 
 <script>
 
 import NavBar from './components/NavBar';
+import Home from'./components/HomePage';
 
 export default {
   name: 'App',
 
   components: {
     NavBar,
+    Home
   },
 
   data: () => ({
